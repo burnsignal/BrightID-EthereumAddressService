7bd0671e3cffd031db9180a07bb69d0f0191eec0
Tomasvrba <14039565+Tomasvrba@users.noreply.github.com>
2019-11-17 16:07:08 +0100

    frontend: move QR code to a pop-up modal dialog

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 // import Input from "./input";
 import BrightEthereumDeepLinkQR from "./qrGenerator";
-import { Button, Form, Input } from 'reactstrap';
+import { Button, Form, Input, Modal, ModalBody, Container } from 'reactstrap';
 
 const Main = () => {
+    // const [modal1, setModal1] = React.useState(false);
     const [input, updateInput] = useState("");
     const [showQR, toggleShowQR] = useState(false);
 
@@ -24,10 +25,6 @@ const Main = () => {
                     <Input onInput={handleChange} id="ethereumAddress" spellCheck={false} autocomplete="off" className="main-input" placeholder="Enter your Ethereum address" />
                 }
                 {
-                    showQR &&
-                    <BrightEthereumDeepLinkQR ethAddress={input} />
-                }
-                {
                     !showQR &&
                     <Button onClick={() => toggleShowQR(!showQR)} size="lg" color="neutral" type="button" disabled={!input}>
                         Submit
@@ -35,9 +32,30 @@ const Main = () => {
                 }
                 {
                     showQR &&
-                    <Button onClick={resetState} size="lg" color="neutral" type="button">
-                        Back
-                    </Button>
+                    <div>
+                        <Modal onExit={resetState} className="QR-modal" isOpen={showQR} toggle={() => toggleShowQR(false)}>
+                            <Container>
+                                <div className="QR-modal-content">
+                                    <ModalBody>
+                                        <div className="QR-modal-content">
+                                            <p>Scan the QR code to connect your Ethereum address with your BrightID account</p>
+                                            <BrightEthereumDeepLinkQR ethAddress={input} />
+                                        </div>
+                                    </ModalBody>
+                                    <div className="modal-footer">
+                                        <Button
+                                            size="lg"
+                                            color="warning"
+                                            type="button"
+                                            onClick={resetState}
+                                        >
+                                            Close
+                                </Button>
+                                    </div>
+                                </div>
+                            </Container>
+                        </Modal>
+                    </div>
                 }
             </div>
         </Form>
