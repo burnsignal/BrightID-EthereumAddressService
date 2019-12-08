23c79fd5dcc64a750fbfac58d52d9f54deeb0265
Tomasvrba <14039565+Tomasvrba@users.noreply.github.com>
2019-12-08 12:53:54 +0100

    frontent: refactor and clean up unsused code

--- src/components/IndexHeader.js
+++ src/components/IndexHeader.js
@@ -35,37 +35,10 @@ function IndexHeader() {
         ></div>
         <Container>
           <div className="content-center brand">
-            {/* <img
-              alt="..."
-              className="n-logo"
-              src={require("assets/img/now-logo.png")}
-            ></img> */}
             <h1 className="h1-seo">Bright Ethereum</h1>
             <h3>Connect your Ethereum address to a global, unique identity network</h3>
             <Main />
           </div>
-          {/* <h6 className="category category-absolute">
-            Designed by{" "}
-            <a href="http://invisionapp.com/?ref=creativetim" target="_blank">
-              <img
-                alt="..."
-                className="invision-logo"
-                src={require("assets/img/invision-white-slim.png")}
-              ></img>
-            </a>
-            . Coded by{" "}
-            <a
-              href="https://www.creative-tim.com?ref=nukr-index-header"
-              target="_blank"
-            >
-              <img
-                alt="..."
-                className="creative-tim-logo"
-                src={require("assets/img/creative-tim-white-slim2.png")}
-              ></img>
-            </a>
-            .
-          </h6> */}
         </Container>
       </div>
     </>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@
 * Now UI Kit React - v1.0.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/now-ui-kit-react
+* Product Page: https://www.creative-tim.com/product/bright-ethereum-react
 * Copyright 2019 Creative Tim (http://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/now-ui-kit-react/blob/master/LICENSE.md)
+* Licensed under MIT (https://github.com/creativetimofficial/bright-ethereum-react/blob/master/LICENSE.md)
 
 * Designed by www.invisionapp.com Coded by www.creative-tim.com
 
@@ -21,34 +21,17 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 // styles for this kit
 import "assets/css/bootstrap.min.css";
-import "assets/scss/now-ui-kit.scss";
+import "assets/scss/bright-ethereum.scss";
 import "assets/demo/demo.css";
 import "assets/demo/nucleo-icons-page-styles.css";
 // pages for this kit
 import Index from "views/Index.js";
-import NucleoIcons from "views/NucleoIcons.js";
-import LoginPage from "views/examples/LoginPage.js";
-import LandingPage from "views/examples/LandingPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Switch>
         <Route path="/index" render={props => <Index {...props} />} />
-        <Route
-          path="/nucleo-icons"
-          render={props => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page"
-          render={props => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={props => <ProfilePage {...props} />}
-        />
-        <Route path="/login-page" render={props => <LoginPage {...props} />} />
         <Redirect to="/index" />
         <Redirect from="/" to="/index" />
       </Switch>
