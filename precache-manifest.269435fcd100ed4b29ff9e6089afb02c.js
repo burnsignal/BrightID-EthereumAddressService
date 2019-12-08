bf6d3ac111b8e2daafd0aaf434b7781ed65f3e48
Tomasvrba <14039565+Tomasvrba@users.noreply.github.com>
2019-12-08 13:47:40 +0100

    Updates

---
+++ precache-manifest.269435fcd100ed4b29ff9e6089afb02c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b02ccca3424eca6c911db89b173315",
+    "url": "/BrightID-EthereumAddressService/index.html"
+  },
+  {
+    "revision": "14100f787618c6ae346b",
+    "url": "/BrightID-EthereumAddressService/static/css/main.3c795abb.chunk.css"
+  },
+  {
+    "revision": "a50c77d086f8eb155e01",
+    "url": "/BrightID-EthereumAddressService/static/js/2.889ef3e7.chunk.js"
+  },
+  {
+    "revision": "14100f787618c6ae346b",
+    "url": "/BrightID-EthereumAddressService/static/js/main.959f2af0.chunk.js"
+  },
+  {
+    "revision": "69fed29a6a84673a4aa4",
+    "url": "/BrightID-EthereumAddressService/static/js/runtime-main.6feace90.js"
+  },
+  {
+    "revision": "a151fee681327157cdc9b7cdd523b940",
+    "url": "/BrightID-EthereumAddressService/static/media/BrightEthereumBackground.a151fee6.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/BrightID-EthereumAddressService/precache-manifest.269435fcd100ed4b29ff9e6089afb02c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BrightID-EthereumAddressService/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
