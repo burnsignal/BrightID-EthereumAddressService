3e88ce0d334c8862f70a5faf74435ff29b2ab05b
Youssef KH <me@youssefkh.com>
2020-04-11 23:36:45 +0100

    remove reverse ens lookup

--- src/util/ens.js
+++ src/util/ens.js
@@ -4,21 +4,10 @@ import ENS from "ethjs-ens";
 const ens = new ENS({ provider: window.web3.currentProvider, network: 1 });
 
 export const convertENS = async (value) => {
-  if( value.includes('.eth') )
-  {
     try {
       const result = await ens.lookup(value);
       return result
     } catch (e) {
       return value
     }
-  }
-  else {
-    try {
-      const resultName = await ens.reverse(value);
-      return resultName
-    } catch (e) {
-      return value;
-    }
-  }
 }
