d4eccb36d8080825b147e08787808c437d3cce9b
Tomasvrba <14039565+Tomasvrba@users.noreply.github.com>
2020-04-14 16:02:40 +0200

    Updates

---
+++ precache-manifest.74180f16fad6706dc543dd0e352434be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754d2c28a8abac5244406ff607171026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0379f98ead7b95559e9e",
+    "url": "/static/css/main.13a6b3c0.chunk.css"
+  },
+  {
+    "revision": "ba96c4beafbb54a2b030",
+    "url": "/static/js/2.25e15b70.chunk.js"
+  },
+  {
+    "revision": "0379f98ead7b95559e9e",
+    "url": "/static/js/main.2221caa9.chunk.js"
+  },
+  {
+    "revision": "fa075f68fa37bfc430c6",
+    "url": "/static/js/runtime-main.3b0b26b7.js"
+  },
+  {
+    "revision": "a151fee681327157cdc9b7cdd523b940",
+    "url": "/static/media/BrightEthereumBackground.a151fee6.png"
+  },
+  {
+    "revision": "dad7dada5be1797a631f109f94f4aa19",
+    "url": "/static/media/MetaMask.dad7dada.svg"
+  }
+]);
