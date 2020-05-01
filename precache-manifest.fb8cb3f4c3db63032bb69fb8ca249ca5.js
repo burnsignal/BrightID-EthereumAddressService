5f9d527dafb01d996376e8a0eda4bde91f148fcd
Tomasvrba <14039565+Tomasvrba@users.noreply.github.com>
2020-05-01 15:53:56 +0200

    add self-sponsorship, built from 3b1d1c2

---
+++ precache-manifest.fb8cb3f4c3db63032bb69fb8ca249ca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f2372e85606aaa9a27840e880adbd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a354018084d8914798b",
+    "url": "/static/css/main.6ddb300a.chunk.css"
+  },
+  {
+    "revision": "9d66a90aa7f6767dd74d",
+    "url": "/static/js/2.386c933c.chunk.js"
+  },
+  {
+    "revision": "5a354018084d8914798b",
+    "url": "/static/js/main.3511c66d.chunk.js"
+  },
+  {
+    "revision": "fa075f68fa37bfc430c6",
+    "url": "/static/js/runtime-main.3b0b26b7.js"
+  },
+  {
+    "revision": "a151fee681327157cdc9b7cdd523b940",
+    "url": "/static/media/BrightEthereumBackground.a151fee6.png"
+  },
+  {
+    "revision": "d861e2f45c27c80e544c9a5cc4f486ec",
+    "url": "/static/media/web3js.d861e2f4.jpg"
+  }
+]);
