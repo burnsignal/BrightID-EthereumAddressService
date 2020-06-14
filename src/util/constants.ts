704fb0b8ca48514b0da22503cfe8028a550a9b5b
Tomasvrba <14039565+Tomasvrba@users.noreply.github.com>
2020-06-14 16:33:41 +0200

    update sponsor contract

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,54 +1,24 @@
-import { AbiItem } from 'web3-utils'
-
+import { AbiItem } from 'web3-utils';
 
 export const BRIGHTID_ENDPOINT = 'https://sp.brightid.org/node/v4/verifications/ethereum';
-export const SPONSOR_CONTRACT_ADDRESS = '0xdCDaDE0E1ecBFEb56cEC05DBe4d04E663b7885ec';
+export const SPONSOR_CONTRACT_ADDRESS = '0xA4DDdF5C53F5FCa4d6901d14FAE463bC02638898';
 export const SPONSOR_CONTRACT_ABI: AbiItem[] = [
   {
+    "anonymous": false, 
     "inputs": [
       {
-        "internalType": "contract BrightID", 
-        "name": "_brightID", 
+        "indexed": false, 
+        "internalType": "address", 
+        "name": "", 
         "type": "address"
-      }, 
-      {
-        "internalType": "bytes32", 
-        "name": "_context", 
-        "type": "bytes32"
       }
     ], 
-    "stateMutability": "nonpayable", 
-    "type": "constructor"
+    "name": "Sponsor", 
+    "type": "event"
   }, 
   {
     "stateMutability": "payable", 
     "type": "fallback"
-  }, 
-  {
-    "inputs": [], 
-    "name": "brightID", 
-    "outputs": [
-      {
-        "internalType": "contract BrightID", 
-        "name": "", 
-        "type": "address"
-      }
-    ], 
-    "stateMutability": "view", 
-    "type": "function"
-  }, 
-  {
-    "inputs": [], 
-    "name": "context", 
-    "outputs": [
-      {
-        "internalType": "bytes32", 
-        "name": "", 
-        "type": "bytes32"
-      }
-    ], 
-    "stateMutability": "view", 
-    "type": "function"
   }, 
   {
     "inputs": [
@@ -62,9 +32,5 @@ export const SPONSOR_CONTRACT_ABI: AbiItem[] = [
     "outputs": [], 
     "stateMutability": "nonpayable", 
     "type": "function"
-  }, 
-  {
-    "stateMutability": "payable", 
-    "type": "fallback"
   }
 ]
