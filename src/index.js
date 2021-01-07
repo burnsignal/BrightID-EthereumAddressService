b23c759c19de969d70b0ae2d2e71cfd8caf0a012
samgos <samuel.jj.gosling@gmail.com>
2021-01-07 17:50:29 +0000

    update dependency conflict

--- src/index.js
+++ src/index.js
@@ -1,65 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import BrightId from 'lib.js'
 
-// styles for this kit
-import "./assets/css/bootstrap.min.css";
-import "./assets/scss/bright-ethereum.scss";
-import "./assets/css/demo.css";
-import "./assets/css/nucleo-icons-page-styles.css";
+function App(){
+   const [ instance, setInstance ] = useState(null)
 
-import { SPONSOR_ADDRESS } from './assets/constants/parameters'
-import { isAddress } from './assets/constants/functions'
-import { isAuthenticated } from './util/brightID'
-import { convertENS } from './util/ens'
+   useEffect(() => {
+     setInstance(new BrightId())
+   }, [])
 
-// pages for this kit
-import Modal from "./components/modal";
-import Index from "./views";
-
-export default class BrightID {
-  constructor(sponsor, sponsorAddress) {
-    switch (sponsor){
-      case true:
-        if(!isAddress(sponsorAddress)){
-          this.sponsor = SPONSOR_ADDRESS;
-        } else {
-          this.sponsor = sponsorAddress;
-        }
-        break;
-      case false:
-        this.sponsor = false;
-        break;
-      default:
-        this.sponsor = SPONSOR_ADDRESS;
-        break;
-    }
-
-    this.isVerified = BrightID.isVerified
-    this.verify = BrightID.verify
-  }
-
-  static verify() {
-    const el = document.createElement("div");
-    el.id = 'brightid-modal';
-
-    document.body.appendChild(el);
-
-    ReactDOM.render(
-      <Modal active>
-        <Index sponsorAddress={this.sponsor}/>
-      </Modal>,
-      document.getElementById(el.id)
-    )
-  }
-
-  static async isVerified(address){
-    if(address.includes('.eth')) {
-      address = await convertENS(address)
-    } else if(address.length != 42){
-      return 'ERROR: INCORRECT ETHEREUM ADDRESS'
-    }
-
-    return await isAuthenticated(address)
-  }
-
+   return(
+     <div>
+      <button onClick={() => instance.verify()}>
+        TRIGGER
+      </button>
+    </div>
+   )
 }
+
+ReactDOM.render(<App />, document.getElementById('root'))
